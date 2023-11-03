27e3c373b7a45ea750b79257c936f3f940a3111f
wanghveganjerky <99945874+wanghveganjerky@users.noreply.github.com>
2023-11-03 05:59:01 -0400

    1

---
+++ main.js
@@ -0,0 +1,19 @@
+function MusicTaste() {
+	let MusicGender = document.querySelectorAll('input[name="gender"]');
+	let ballad = document.querySelector("#vanilla");
+	let Pop = document.querySelector("#Pop");
+	let Rb = document.querySelector("#RB");
+	let text = document.querySelector("#text");
+
+	let selectedMusic;
+	for (const radioButton of MusicGender) {
+		if (radioButton.checked) {
+			selectedMusic = radioButton.value;
+		}
+	}
+
+	// show the output:
+	text.innerHTML = selectedMusic
+		? `You selected disc ${selectedMusic}`
+		: `You haven't selected any disc`;
+}
