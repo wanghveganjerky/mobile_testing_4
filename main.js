46f6edf3420659ca55c54747f2d447ad8785deaf
wanghveganjerky <99945874+wanghveganjerky@users.noreply.github.com>
2023-11-03 06:47:46 -0400

    1

--- main.js
+++ main.js
@@ -17,3 +17,21 @@ function MusicTaste() {
 		? `Currently playing: disc ${selectedMusic}`
 		: `You haven't selected any disc`;
 }
+
+function updateDisc() {
+    var discSelect = document.getElementById("discSelect");
+    var vinylImage = document.getElementById("vinylImage");
+    var discAudio = document.getElementById("discAudio");
+
+    // Update the image source
+    vinylImage.src = "images/" + discSelect.value;
+
+    // Update the audio source and play the audio
+    discAudio.src = "images/" + discSelect.value.replace('png', 'mp3');
+    discAudio.load(); // This is necessary to load the new source
+    discAudio.play();
+}
+
+
+
+
